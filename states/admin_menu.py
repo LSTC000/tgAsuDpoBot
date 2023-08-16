e06e9b259ba00521af05b7516370902e44b6f7f5
LSTC000 <razgonyaev_74@mail.ru>
2023-08-16 10:38:04 +0700

    update admin_menu.py

--- states/admin_menu.py
+++ states/admin_menu.py
@@ -3,5 +3,5 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class AdminMenuStatesGroup(StatesGroup):
     admin_menu = State()
-    alert_for_users = State()
+    enter_alert_for_users = State()
     confirm_alert_for_users = State()
