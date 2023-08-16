b1ed287bd8adb31cc1628c790251d543d6ee02ef
LSTC000 <razgonyaev_74@mail.ru>
2023-08-16 10:37:47 +0700

    update bot.py

--- bot.py
+++ bot.py
@@ -11,24 +11,22 @@ from handlers import (
     register_users_commands,
     register_users_main_menu,
     register_users_cancels,
-    register_users_payment,
     register_admins_commands,
-    register_admin_menu
+    register_admins_alert_for_users,
 )
 
 from database import startup_setup, shutdown_setup
 
-from functions import send_alerts
+from functions import send_alert
 
 from aiogram import Dispatcher
 from aiogram.utils import executor
 
 
 def register_all_handlers(dispatcher: Dispatcher):
-    register_users_payment(dispatcher)
     register_users_cancels(dispatcher)
     register_users_main_menu(dispatcher)
-    register_admin_menu(dispatcher)
+    register_admins_alert_for_users(dispatcher)
     register_users_commands(dispatcher)
     register_admins_commands(dispatcher)
 
@@ -49,12 +47,12 @@ async def on_startup(dispatcher: Dispatcher):
     register_all_handlers(dispatcher)
 
     logger.info('Bot starting users alert')
-    await send_alerts(text_alert=ALERT_STARTUP_MESSAGE)
+    await send_alert(text_alert=ALERT_STARTUP_MESSAGE)
 
 
 async def on_shutdown(dispatcher: Dispatcher):
     logger.info('Bot stopped users alert')
-    await send_alerts(text_alert=ALERT_SHUTDOWN_MESSAGE)
+    await send_alert(text_alert=ALERT_SHUTDOWN_MESSAGE)
 
     logger.info('Closing PostgreSQL connection')
     await shutdown_setup()
