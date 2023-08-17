543a3069523271e0db247056d6202713e4347cb7
LSTC000 <razgonyaev_74@mail.ru>
2023-08-17 17:53:43 +0700

    update alerts.py

--- database/schemas/alerts.py
+++ database/schemas/alerts.py
@@ -1,6 +1,6 @@
 from database.database_setup import BaseModel
 
-from sqlalchemy import Column, BigInteger, sql
+from sqlalchemy import Column, BigInteger, sql, DateTime, func
 
 
 class Alerts(BaseModel):
@@ -8,5 +8,7 @@ class Alerts(BaseModel):
 
     # Telegram user id.
     user_id = Column(BigInteger, nullable=False, primary_key=True)
+    # Created review date.
+    created_date = Column(DateTime(True), server_default=func.now())
 
     query: sql.select
