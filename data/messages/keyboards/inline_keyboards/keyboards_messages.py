9134bc99a7d5ca9fd8ccba9a18f5d9ded894fe6a
LSTC000 <razgonyaev_74@mail.ru>
2023-08-17 17:48:18 +0700

    update keyboards_messages.py

--- data/messages/keyboards/inline_keyboards/keyboards_messages.py
+++ data/messages/keyboards/inline_keyboards/keyboards_messages.py
@@ -1,12 +1,11 @@
 # CANCELS.
 CANCEL_TO_MAIN_MENU_IKB_MESSAGE = '⬅️ Вернуться в главное меню'
+CANCEL_TO_ADMIN_MENU_IKB_MESSAGE = '⬅️ Вернуться в меню админимстратора'
 # USERS COMMANDS.
 START_COMMAND_IKB_MESSAGE = 'Давай начнём 🚀'
 # MAIN MENU.
 USER_ALERT_ON_IKB_MESSAGE = '🔔 Включить уведомления'
 USER_ALERT_OFF_IKB_MESSAGE = '🔕 Отключить уведомления'
-OFFICIAL_GROUP_URL_IKB_MESSAGE = '🚀 Наша группа'
-PAYMENT_IKB_MESSAGE = '💰 Поддержать проект'
 # ADMIN MENU.
 ALERT_FOR_USERS_IKB_MESSAGE = '🗞️ Отправить объявление'
 CONFIRM_ALERT_FOR_USERS_IKB_MESSAGE = '✅ Да'
