195b41327ed311289bc96864f211dad7f0c9aec5
LSTC000 <razgonyaev_74@mail.ru>
2023-08-17 19:25:20 +0700

    update update_answer.py

--- database/commands/update_answer.py
+++ database/commands/update_answer.py
@@ -1,11 +1,11 @@
 from database import QuestionsAndAnswers
 
 
-async def update_answer(question: str, new_answer: str) -> None:
+async def update_answer(question_id: int, new_answer: str) -> None:
     """
-    :param question: Question.
+    :param question_id: Question id.
     :param new_answer: New answer.
     """
 
     await QuestionsAndAnswers.update.values(answer=new_answer) \
-        .where(QuestionsAndAnswers.question == question).gino.status()
+        .where(QuestionsAndAnswers.id == question_id).gino.status()
