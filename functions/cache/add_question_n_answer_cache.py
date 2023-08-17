4e335950975f03fc75c2df250079a516357db801
LSTC000 <razgonyaev_74@mail.ru>
2023-08-17 19:48:47 +0700

    update add_question_n_answer_cache.py

--- functions/cache/add_question_n_answer_cache.py
+++ functions/cache/add_question_n_answer_cache.py
@@ -10,7 +10,7 @@ async def add_question_n_answer_cache(question: str, answer: str) -> None:
     :return: None.
     """
 
-    # Add question and answer the database.
+    # Add question and answer in the database.
     await add_question_n_answer(question=question, answer=answer)
     # Update questions cache.
     questions_cache['questions'] = {question[0]: question[1] for question in await get_questions()}
