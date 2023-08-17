2be334569e71c64f73e8a483be5f728360f966eb
LSTC000 <razgonyaev_74@mail.ru>
2023-08-17 17:48:37 +0700

    update main_menu_ikb.py

--- keyboards/inline_keyboards/users/main_menu/main_menu_ikb.py
+++ keyboards/inline_keyboards/users/main_menu/main_menu_ikb.py
@@ -1,18 +1,8 @@
 from data.config import ROW_WIDTH
 
-from data.urls import OFFICIAL_GROUP_URL
+from data.callbacks import CHANGE_USER_ALERT_CALLBACK_DATA
 
-from data.callbacks import (
-    CHANGE_USER_ALERT_CALLBACK_DATA,
-    PAYMENT_CALLBACK_DATA
-)
-
-from data.messages import (
-    USER_ALERT_ON_IKB_MESSAGE,
-    USER_ALERT_OFF_IKB_MESSAGE,
-    OFFICIAL_GROUP_URL_IKB_MESSAGE,
-    PAYMENT_IKB_MESSAGE
-)
+from data.messages import USER_ALERT_ON_IKB_MESSAGE, USER_ALERT_OFF_IKB_MESSAGE
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -29,7 +19,5 @@ def main_menu_ikb(alert: bool) -> InlineKeyboardMarkup:
         text=USER_ALERT_OFF_IKB_MESSAGE if alert else USER_ALERT_ON_IKB_MESSAGE,
         callback_data=CHANGE_USER_ALERT_CALLBACK_DATA)
     )
-    ikb.row(InlineKeyboardButton(text=OFFICIAL_GROUP_URL_IKB_MESSAGE, url=OFFICIAL_GROUP_URL))
-    ikb.row(InlineKeyboardButton(text=PAYMENT_IKB_MESSAGE, callback_data=PAYMENT_CALLBACK_DATA))
 
     return ikb
