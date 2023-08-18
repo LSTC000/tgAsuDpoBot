37b898382e14e8de74b6bf50d87c7e80813e8ec8
LSTC000 <razgonyaev_74@mail.ru>
2023-08-18 21:48:56 +0700

    update process_delete_question_n_answer_picker.py

--- handlers/admins/questions_n_answers/delete_question_n_answer/process_delete_question_n_answer_picker.py
+++ handlers/admins/questions_n_answers/delete_question_n_answer/process_delete_question_n_answer_picker.py
@@ -1,8 +1,8 @@
 from loader import dp
 
-from data.redis import QUESTION_REDIS_KEY, QUESTION_ID_REDIS_KEY
+from data.redis import QUESTION_ID_REDIS_KEY
 
-from functions import clear_last_ikb, get_questions_cache, call_confirm_delete_question_n_answer_menu_ikb
+from functions import clear_last_ikb, call_confirm_delete_question_n_answer_menu_ikb
 
 from pickers import QuestionsAndAnswersPicker
 
@@ -23,18 +23,12 @@ async def process_delete_question_n_answer_picker(callback: types.CallbackQuery,
     )
 
     if choice:
-        # Clear last inline keyboard.
-        await clear_last_ikb(user_id=user_id, state=state)
-
-        questions = await get_questions_cache()
-        question_id = int(question_id)
-        question = questions[question_id]
-
         async with state.proxy() as data:
-            data[QUESTION_REDIS_KEY] = question
             data[QUESTION_ID_REDIS_KEY] = question_id
 
-        # Call questions and answers picker inline menu.
+        # Clear last inline keyboard.
+        await clear_last_ikb(user_id=user_id, state=state)
+        # Call confirm delete question and answer inline menu.
         await call_confirm_delete_question_n_answer_menu_ikb(user_id=user_id, state=state)
         # Set question_n_answer_report state.
         await DeleteQuestionAndAnswerStatesGroup.confirm_delete_question_n_answer_menu.set()
