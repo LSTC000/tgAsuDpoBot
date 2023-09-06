671f9b6e4cbfef578494cc54afab4c0d93ca7d14
LSTC000 <razgonyaev_74@mail.ru>
2023-09-06 10:42:12 +0700

    update redis_config.py

--- data/config/redis/redis_config.py
+++ data/config/redis/redis_config.py
@@ -1,3 +1,10 @@
-REDIS_HOST = 'localhost'
+import os
+
+from dotenv import load_dotenv
+from dotenv import find_dotenv
+
+load_dotenv(find_dotenv())
+
+REDIS_HOST = os.getenv('PREDIS_HOST')
 REDIS_PORT = 6379
 REDIS_DB = 1
